089d8692bd333805896c7a8e64c9f759e6fa9c50
ahmet şemsettin özdemirden <ahmetozdemirden@gmail.com>
2019-06-26 17:52:23 +0300

    add stage as cross origin

--- src/main/java/com/iyteemlak/api/controller/HouseController.java
+++ src/main/java/com/iyteemlak/api/controller/HouseController.java
@@ -21,14 +21,14 @@ public class HouseController {
     }
 
     @GetMapping(produces = "application/json")
-    @CrossOrigin(origins = {"http://localhost:3000", "http://www.iyteemlak.com"})
+    @CrossOrigin(origins = {"http://localhost:3000", "http://www.iyteemlak.com", "http://stage.iyteemlak.com"})
     public List<HouseDTO> getAllHouses() {
         return houseService.getAllHouses();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @CrossOrigin(origins = {"http://localhost:3000", "http://www.iyteemlak.com"})
+    @CrossOrigin(origins = {"http://localhost:3000", "http://www.iyteemlak.com", "http://stage.iyteemlak.com"})
     public void addHouse(@RequestBody AddHouseRequest addHouseRequest) {
         houseService.addHouse(addHouseRequest);
     }
