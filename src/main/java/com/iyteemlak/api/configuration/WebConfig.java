e3af7d5e9a92944dd05ea22d4167cdd05fcee0c1
ahmet şemsettin özdemirden <ahmetozdemirden@gmail.com>
2019-07-01 17:18:10 +0300

    fix whitelabel error page problem

--- src/main/java/com/iyteemlak/api/configuration/WebConfig.java
+++ src/main/java/com/iyteemlak/api/configuration/WebConfig.java
@@ -2,16 +2,14 @@ package com.iyteemlak.api.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/house")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://www.iyteemlak.com", "http://stage.iyteemlak.com");
     }
 
